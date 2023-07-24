357ed539c882a2b62f0c70781e35ac8e9d02548b
Ekta549 <133002329+Ekta549@users.noreply.github.com>
2023-07-24 13:24:50 +0530

    Update homepage.dart

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -65,7 +65,7 @@ class HomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'asset/demo1.png', // Fixed the asset path here
+              'asset/flutter.jpg', // Fixed the asset path here
             ),
             const SizedBox(height: 7),
             const Text(
